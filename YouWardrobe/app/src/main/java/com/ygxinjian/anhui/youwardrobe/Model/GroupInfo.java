321141b59f08f64d43b252176b88de3e05ae4487
暗尘随码去 <353114440@qq.com>
2017-03-21 18:13:13 +0800

    衣柜

---
+++ YouWardrobe/app/src/main/java/com/ygxinjian/anhui/youwardrobe/Model/GroupInfo.java
@@ -0,0 +1,16 @@
+package com.ygxinjian.anhui.youwardrobe.Model;
+
+public class GroupInfo extends BaseInfo
+{
+	public GroupInfo()
+	{
+		super();
+	}
+
+	public GroupInfo(String id, String name)
+	{
+		super(id, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
