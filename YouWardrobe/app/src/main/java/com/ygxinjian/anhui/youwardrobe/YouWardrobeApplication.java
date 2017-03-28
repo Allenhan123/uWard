037a661761c490b78f61848f59b59e03ff0d8eec
yangyonglu <960611948@qq.com>
2017-03-28 09:20:24 +0800

    添加keystore 签名文件

--- YouWardrobe/app/src/main/java/com/ygxinjian/anhui/youwardrobe/YouWardrobeApplication.java
+++ YouWardrobe/app/src/main/java/com/ygxinjian/anhui/youwardrobe/YouWardrobeApplication.java
@@ -2,6 +2,9 @@ package com.ygxinjian.anhui.youwardrobe;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.util.Log;
 
 import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
 import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
@@ -13,6 +16,9 @@ import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.log.LoggerInterceptor;
 
 import java.io.File;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
@@ -22,6 +28,7 @@ import okhttp3.OkHttpClient;
  */
 
 public class YouWardrobeApplication extends Application {
+    private static final String TAG = "YouWardrobeApplication";
     private static final int MEMORY_SIZE = 5 * 1024 * 1024;
     private static final int DISK_SIZE = 20 * 1024 * 1024;
     public static Context sContext;
@@ -63,9 +70,37 @@ public class YouWardrobeApplication extends Application {
                 .build();
 
         OkHttpUtils.initClient(okHttpClient);
+        Log.d(TAG, "onCreate: "+sHA1(getApplicationContext()));
     }
 
     public static Context getAppContext(){
         return sContext;
+    }
+
+
+    public static String sHA1(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), PackageManager.GET_SIGNATURES);
+            byte[] cert = info.signatures[0].toByteArray();
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(cert);
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < publicKey.length; i++) {
+                String appendString = Integer.toHexString(0xFF & publicKey[i])
+                        .toUpperCase(Locale.US);
+                if (appendString.length() == 1)
+                    hexString.append("0");
+                hexString.append(appendString);
+                hexString.append(":");
+            }
+            String result= hexString.toString();
+            return result.substring(0, result.length()-1);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
