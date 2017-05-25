5b5a6ee808c008e15fa3f4c3ae52ce6ffaf3bc7c
暗尘随码去 <353114440@qq.com>
2017-05-25 11:55:16 +0800

    更多特权

--- YouWardrobe/app/src/main/java/com/ygxinjian/anhui/youwardrobe/Fragment/Fragment_Vip_Month.java
+++ YouWardrobe/app/src/main/java/com/ygxinjian/anhui/youwardrobe/Fragment/Fragment_Vip_Month.java
@@ -1,12 +1,16 @@
 package com.ygxinjian.anhui.youwardrobe.Fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.ygxinjian.anhui.youwardrobe.R;
+import com.ygxinjian.anhui.youwardrobe.utils.DevUtil;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -24,9 +28,49 @@ public class Fragment_Vip_Month extends BaseFragment {
     @InjectView(R.id.cardView_months_3)
     CardView cardViewMonths3;
 
+    private  BottomSheetBehavior sheetBehavior;
+    private LinearLayout ll_wechat_pay,ll_ali_pay;
+
     @Override
     public View initView() {
         View view = View.inflate(mActivity, R.layout.fragment_vip_months, null);
+        View shareView = mActivity.findViewById(R.id.share_view);
+        //获取BottomSheetBehavior
+        sheetBehavior = BottomSheetBehavior.from(shareView);
+
+        ll_ali_pay = (LinearLayout) shareView.findViewById(R.id.ll_ali_pay);
+        ll_wechat_pay = (LinearLayout) shareView.findViewById(R.id.ll_wechat_pay);
+        ll_wechat_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DevUtil.showInfo(mActivity,"微信支付");
+            }
+        });
+        ll_ali_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DevUtil.showInfo(mActivity,"支付宝支付");
+            }
+        });
+        //设置折叠时的高度
+        //sheetBehavior.setPeekHeight(BottomSheetBehavior.PEEK_HEIGHT_AUTO);
+
+        //监听BottomSheetBehavior 状态的变化
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+        //下滑的时候是否可以隐藏
+        sheetBehavior.setHideable(true);
+
         return view;
     }
 
@@ -49,6 +93,11 @@ public class Fragment_Vip_Month extends BaseFragment {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.cardView_months_1:
+                if(sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED){
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }else {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                }
                 break;
             case R.id.cardView_months_2:
                 break;
